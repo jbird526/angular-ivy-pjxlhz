e4a6544d1f8bf59b4080ce9d5a394d67425e875d
jbird526 <jpecsek@gmail.com>
2022-08-23 14:50:16 -0400

    3 files modified

--- src/app/gantt-chart/gantt-chart.module.ts
+++ src/app/gantt-chart/gantt-chart.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GanttChartControlComponent } from './gantt-chart-control/gantt-chart-control.component';
-
-
+import { MaterialsModule } from '../../materials.module';
 
 @NgModule({
   declarations: [GanttChartControlComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [GanttChartControlComponent]
+  imports: [CommonModule, MaterialsModule],
+  exports: [GanttChartControlComponent],
 })
-export class GanttChartModule { }
+export class GanttChartModule {}
