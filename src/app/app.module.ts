89d0078db39cbd63b7f000c12b48d79b9add8a98
jbird526 <jpecsek@gmail.com>
2022-08-23 13:22:00 -0400

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,12 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { GanttChartModule } from './gantt-chart/gantt-chart.module';
 
+// import { MatExpansionModule } from '@angular/material/expansion';
+
+import { MaterialsModule } from './materials.module';
+
 @NgModule({
-  imports: [BrowserModule, FormsModule, GanttChartModule],
+  imports: [BrowserModule, FormsModule, GanttChartModule, MaterialsModule],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
 })
--- src/app/gantt-chart/gantt-chart-control/gantt-chart-control.component.ts
+++ src/app/gantt-chart/gantt-chart-control/gantt-chart-control.component.ts
@@ -7,7 +7,7 @@ import { DateHelperService } from '../_services/date-helper.service';
 @Component({
   selector: 'app-gantt-chart-control',
   templateUrl: './gantt-chart-control.component.html',
-  styleUrls: ['./gantt-chart-control.component.scss']
+  styleUrls: ['./gantt-chart-control.component.scss'],
 })
 export class GanttChartControlComponent implements OnInit {
   @Input() rows: IGanttCharRow[];
@@ -15,29 +15,39 @@ export class GanttChartControlComponent implements OnInit {
   @Input() endDate: Date = new Date('2021-12-30');
   chartPeriodDays: number;
   monthAxis: MonthAxis[];
-  colourPallete = ['#7C4DFF',
+  colourPallete = [
+    '#7C4DFF',
     '#81c784',
     '#e53935',
     '#FF8A80',
     '#303F9F',
     '#40C4FF',
     '#006064',
-    '#FF8A65']
+    '#FF8A65',
+  ];
 
   constructor() {
-    this.chartPeriodDays = DateHelperService.dateDifference(this.endDate, this.startDate, true);
+    this.chartPeriodDays = DateHelperService.dateDifference(
+      this.endDate,
+      this.startDate,
+      true
+    );
     this.monthAxis = this.getMonths(this.startDate, this.endDate);
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   getEventDurationPercentage(event: IGanttChartEvent): number {
-    const eventDays = DateHelperService.dateDifference(event.endDate, event.startDate);
+    const eventDays = DateHelperService.dateDifference(
+      event.endDate,
+      event.startDate
+    );
     return (eventDays / this.chartPeriodDays) * 100;
-
   }
   getEventOffsetPercentage(eventStartDate: Date): number {
-    const daysPriorToEventStart = DateHelperService.dateDifference(eventStartDate, this.startDate);
+    const daysPriorToEventStart = DateHelperService.dateDifference(
+      eventStartDate,
+      this.startDate
+    );
     return ((daysPriorToEventStart - 1) / this.chartPeriodDays) * 100;
   }
   getMonths(startDate: Date, endDate: Date): MonthAxis[] {
@@ -45,21 +55,28 @@ export class GanttChartControlComponent implements OnInit {
     console.log(endDate);
     const startMonth = startDate.getMonth();
     const endMonth = endDate.getMonth();
-    const totalDurationDays = DateHelperService.dateDifference(startDate, endDate, true);
+    const totalDurationDays = DateHelperService.dateDifference(
+      startDate,
+      endDate,
+      true
+    );
     let months: MonthAxis[] = new Array();
     for (var i = 0; i <= endMonth - startMonth; i++) {
       const adjustedStartDate = DateHelperService.addMonths(startDate, i);
       const monthName = DateHelperService.getMonthName(adjustedStartDate);
       const daysInMonth = DateHelperService.daysInMonth(adjustedStartDate);
-      const monthDurationPercentage = daysInMonth / totalDurationDays * 100;
-      months.push({ monthName: monthName, monthDurationPercentage: monthDurationPercentage });
+      const monthDurationPercentage = (daysInMonth / totalDurationDays) * 100;
+      months.push({
+        monthName: monthName,
+        monthDurationPercentage: monthDurationPercentage,
+      });
     }
     return months;
   }
   getColour(rowIndex: number, eve): string {
     console.log(eve);
     if (eve.color) {
-      return eve.color
+      return eve.color;
     } else {
       if (this.rows.length < rowIndex) {
         return '#64b5f6';
@@ -67,8 +84,5 @@ export class GanttChartControlComponent implements OnInit {
 
       return this.colourPallete[rowIndex];
     }
-
   }
-
 }
-
